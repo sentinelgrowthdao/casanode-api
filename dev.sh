af6101441802ae743487c6f8f32862f56c7fd2dd
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-09-11 22:44:15 +0200

    fix: remove unnecessary Bluetooth dependencies and related commands from dev.sh

--- dev.sh
+++ dev.sh
@@ -12,7 +12,7 @@ sudo sudo apt install build-essential -y
 # Install Node.js
 echo "Installing Node.js"
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt install -y nodejs
-sudo apt install bluetooth bluez libbluetooth-dev libudev-dev libcap2-bin
+sudo apt install libudev-dev
 
 # Make log directory
 echo "Creating log directory"
@@ -30,7 +30,3 @@ echo "deb-src http://ppa.launchpad.net/foxinou/dvpn-node-manager/ubuntu jammy ma
 gpg --keyserver keyserver.ubuntu.com --recv-keys 1E4DCBDC436F95468F1FEB793B604A1F5EE3D8E5 && gpg --export 1E4DCBDC436F95468F1FEB793B604A1F5EE3D8E5 | sudo tee /etc/apt/trusted.gpg.d/foxinou_dvpn-node-manager.gpg > /dev/null
 sudo apt update
 sudo apt install -y dvpn-node-manager
-
-# Run setcap command to grant cap_net_raw privileges to Node.js
-sudo apt install -y libcap2-bin
-sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
